73525f3132993296ef57e72e883f9e1ace2163d8
robinrogin-maker <robinrogin@gmail.com>
2026-09-06 21:29:38 +0200

    Update Instagram link in social media section

--- data.js
+++ data.js
@@ -14,7 +14,7 @@ const SITE = {
   // إلى حسابات الموقع الحقيقية. الزر الذي رابطه فارغ لا يظهر إطلاقاً.
   social: {
     // روابط حسابات المالك؛ أي رابط يُترك فارغاً يُخفي زرّه تماماً.
-    instagram: "https://www.instagram.com/adel_shirwani/",
+    instagram: "https://www.instagram.com/shirwaniadel/?hl=en",
     tiktok:    "https://www.tiktok.com/@adelshirwani4",
     x:         "https://x.com/ShirwaniAdel"
   }
